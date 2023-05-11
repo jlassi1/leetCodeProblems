92971c9e5d61d5b034a3c2cd72fefc4be02ee2b0
khawla jlassi <57707783+jlassi1@users.noreply.github.com>
2023-05-11 12:51:35 +0100

    Time: 89 ms (26.25%), Space: 16.4 MB (11.97%) - LeetHub

---
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.py
@@ -0,0 +1,12 @@
+class Solution:
+    def processQueries(self, queries: List[int], m: int) -> List[int]:
+        p = [i for i in range(1, m+1)]
+        x = 0
+        for idx, value in enumerate(queries):
+            idx_p = p.index(value)
+            queries[idx] = idx_p
+            p = [value] + p[0:idx_p] + p[idx_p+1:]
+            
+
+        
+        return queries
